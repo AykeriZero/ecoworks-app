6ac96438bfc99257e581539b74e3d47453cf624e
songya <songya@umich.edu>
2018-11-28 18:45:55 -0500

    added redux starter code

--- Ecoworks/src/App.js
+++ Ecoworks/src/App.js
@@ -1,49 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import reducers from './reducers';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class App extends Component {
 
-type Props = {};
-export default class App extends Component<Props> {
   render() {
+    const store = createStore(reducers);
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Provider store={store}>
+        <Text>Hello World</Text>
+      </Provider>
     );
   }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default App;
---
+++ Ecoworks/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+  // combine reducers needs at least one reducer,
+  // delete this once we have actual reducers
+  HOLDER_DELETE_THIS: () => []
+});
