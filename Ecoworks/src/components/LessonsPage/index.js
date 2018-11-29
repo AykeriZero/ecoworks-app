43ad0cac8e492786a1ee975dd5ea0422eef1939e
songya <songya@umich.edu>
2018-11-28 19:25:30 -0500

    inital Lessons page stuffs

--- Ecoworks/src/components/LessonsPage/index.js
+++ Ecoworks/src/components/LessonsPage/index.js
@@ -1,1 +1,14 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class LessonsPage extends Component {
+  render() {
+    return (
+      <View>
+        <Text>LessonsPage</Text>
+      </View>
+    );
+  }
+}
+
+export default LessonsPage;
