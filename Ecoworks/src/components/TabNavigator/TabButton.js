06dcf2352409069032b3b35b0c6a38c533e3f5d1
songya <songya@umich.edu>
2019-02-06 12:10:31 -0500

    removed usused import

--- Ecoworks/src/components/TabNavigator/TabButton.js
+++ Ecoworks/src/components/TabNavigator/TabButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, View, Text, TouchableWithoutFeedback } from 'react-native';
+import { SafeAreaView, Text, TouchableWithoutFeedback } from 'react-native';
 
 const TabButton = ({ onPress, page, selected }) => {
   if (selected) {
@@ -36,7 +36,7 @@ const styles = {
     alignSelf: 'stretch',
     backgroundColor: '#fff',
     borderRadius: 5,
-    borderWidth: 1,
+    borderWidth: 2,
     borderColor: '#007aff',
     marginLeft: 5,
     marginRight: 5
