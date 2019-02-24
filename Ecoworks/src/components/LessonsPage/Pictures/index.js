79b5f57132399a6c982127e70aa1b9c435d4258b
Noa Ben-Efraim <noa.benefraim@Noas-MacBook-Pro.local>
2019-02-23 20:19:51 -0500

    trying to merge lessons noa and sarah

--- Ecoworks/src/components/LessonsPage/Pictures/index.js
+++ Ecoworks/src/components/LessonsPage/Pictures/index.js
@@ -1,9 +1,12 @@
 const img1 = require('./lp1-chess.png');
+const img4 = require('./airLeak.png')
 
 function getImage(image) {
   switch (image) {
     case 1:
       return img1;
+    case 4:
+      return img4;
     default:
       return img1;
   }
