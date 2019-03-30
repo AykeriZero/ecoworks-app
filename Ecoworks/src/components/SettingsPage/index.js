66d1828a5544defc72f47b9d0dd0785301957358
skunkel <skunkel@umich.edu>
2019-03-29 20:48:44 -0400

    ToggleSwitch

--- Ecoworks/src/components/SettingsPage/index.js
+++ Ecoworks/src/components/SettingsPage/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import ToggleSwitch from 'toggle-switch-react-native';
 
-import {
-    Text,
-    View,
-    FlatList
-} from 'react-native';
-
-
+// import {
+//     Text
+// } from 'react-native';
 
 class SettingsPage extends React.Component {
   render() {
-    return ({});
+    return (<ToggleSwitch
+        isOn
+        onColor='green'
+        offColor='red'
+        label='English'
+        labelStyle={{ color: 'black', fontWeight: '900' }}
+        size='large'
+        onToggle={(isOn) => console.log('changed to : ', isOn)}
+    />);
   }
 }
 
