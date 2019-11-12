93b2f096b6f9df26eaaf8d7734e1b3a9f992e583
Noa Ben-Efraim <noa.benefraim@Noas-MacBook-Pro.local>
2019-11-11 19:40:28 -0500

    noa + sarah take 2

---
+++ Ecoworks/src/components/LessonsPage/Pages/LessonPage8.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import { ScrollView, Text, Image } from 'react-native';
+
+import { Card, CardSection, TitleFont } from '../../common';
+//const Furnace_img = require('./Furnace.png');
+import getImage from '../../../pictures';
+
+import englishData from './en/en_Lesson8.json';
+import spanishData from './sp/sp_Lesson8.json';
+
+class LessonPage8 extends Component {
+
+    render() {
+        let data = englishData; // default to english
+        if (true) {
+            data = spanishData;
+        }
+        return (
+          <ScrollView>
+
+                <Card>
+                <CardSection>
+                <TitleFont>{data.description.title}</TitleFont>
+                </CardSection>
+                </Card>
+
+
+                <Card>
+                <CardSection>
+                <TitleFont>{data.description.title2}</TitleFont>
+                </CardSection>
+                <CardSection>
+                <Text>{data.description.body1}{data.description.body2}{data.description.body3}{data.description.body4}</Text>
+                </CardSection>
+                </Card>
+
+
+                <Card>
+                <CardSection>
+                <TitleFont>{data.description.title3}</TitleFont>
+                </CardSection>
+                <CardSection>
+                <Text>{data.description.body5}{data.description.body6}{data.description.body7}{data.description.body8}</Text>
+                </CardSection>
+                </Card>
+
+                <Card>
+                <CardSection>
+                <TitleFont>{data.description.title4}</TitleFont>
+                </CardSection>
+                <CardSection>
+                <Text>{data.description.body9}{data.description.body10}{data.description.body11}{data.description.body12}</Text>
+                </CardSection>
+                </Card>
+
+                <Card>
+                <CardSection>
+                <TitleFont>{data.description.title5}</TitleFont>
+                </CardSection>
+                <CardSection>
+                <Text>{data.description.body13}{data.description.body14}</Text>
+                </CardSection>
+                </Card>
+
+                <Card>
+                <CardSection>
+                <TitleFont>{data.description.title6}</TitleFont>
+                </CardSection>
+                <CardSection>
+                <Text>{data.description.body15}{data.description.body16}</Text>
+                <CardSection>
+                  <Image
+                    style={styles.imageStyle}
+                    source={getImage(data.lights.jpg)}
+                    resizeMode={'contain'}
+                  />
+                  </CardSection>
+                </CardSection>
+                </Card>
+
+
+        </ScrollView>
+      );
+    }
+}
+
+const styles = {
+titleCardStyle: {
+justifyContent: 'center'
+},
+imageStyle: {
+height: 300,
+flex: 1,
+width: null
+}
+};
+
+const mapStateToPros = (state) => {
+  return {settings: state.settings};
+}
+export default connect( mapStateToPros, {})(LessonPage8);
