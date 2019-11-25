9103a75ea45606e6785b23fb2154e2e76bd2285f
Andrew Yuan <yuandrew@umich.edu>
2019-11-24 21:08:54 -0500

    Finished resources page draft

--- Ecoworks/src/components/ResourcesPage/index.js
+++ Ecoworks/src/components/ResourcesPage/index.js
@@ -3,12 +3,45 @@ import React from 'react';
 import {
     Text,
     View,
+    FlatList
 } from 'react-native';
 
 import { CardSection, Card, TitleFont } from '../common';
 
+import data from './resources.json';
+
 class ResourcesPage extends React.Component {
     /* Display the main bills page */
+    // function Resource({ item }) {
+    Resource = ({ item }) => {
+      return (
+          <Card>
+
+            <CardSection>
+              <Text style={{fontWeight: "bold"}}> Agency:</Text>
+              <Text> {item.agency} </Text>
+            </CardSection>
+
+            <CardSection>
+              <Text style={{fontWeight: "bold"}}> Phone Number:</Text>
+              <Text> {item.phone} </Text>
+            </CardSection>
+
+            <CardSection>
+              <Text style={{fontWeight: "bold"}}> Website:</Text>
+              <Text> {item.website} </Text>
+            </CardSection>
+
+            <CardSection>
+              <Text style={{fontWeight: "bold"}}> Text:</Text>
+              <Text> {item.comments} </Text>
+            </CardSection>
+
+            <Text>{'\n'}</Text>
+
+          </Card>
+      );
+    }
 
     render() {
       return (
@@ -16,18 +49,16 @@ class ResourcesPage extends React.Component {
 
         <Card>
           <CardSection>
-              <TitleFont> Bills </TitleFont>
+              <TitleFont> Additional Resources </TitleFont>
           </CardSection>
 
         </Card>
 
-        <Card>
-          <CardSection>
-              {
-                  // TODO: DON'T USE CHECKBOX, USE SWITCH
-              }
-          </CardSection>
-        </Card>
+        <FlatList
+          data={data.resources}
+          renderItem={this.Resource}
+          keyExtractor={item => item.agency}
+        />
 
         </View>
       );
