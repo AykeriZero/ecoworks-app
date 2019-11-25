790bd244b0b032fdaaeb01ed4193c67b25fafaf7
Andrew Yuan <yuandrew@umich.edu>
2019-11-24 23:03:10 -0500

    began bill display page

--- Ecoworks/src/components/LessonsPage/Pages/LessonPage7.js
+++ Ecoworks/src/components/LessonsPage/Pages/LessonPage7.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { ScrollView, Text, Image } from 'react-native';
+import React, { Component, PropTypes } from 'react';
+import { ScrollView, Text, Image, View } from 'react-native';
 
 import { connect } from 'react-redux';
 
@@ -9,9 +9,42 @@ import getImage from '../../../pictures';
 import englishData from './en/en_Lesson7.json';
 import spanishData from './sp/sp_Lesson7.json';
 
+
+import ZoomableImage from './react-native-interactive-image.js';
+
 class LessonPage7 extends Component {
 
   render() {
+      const annotations = [
+	{
+		x1: 25,
+		x2: 35,
+		y1: 20,
+		y2: 30,
+		description: 'A pair of black running sports shoes, has lace-up detail. Textile and mesh upper',
+	},
+	{
+		x1: 60,
+		x2: 70,
+		y1: 15,
+		y2: 25,
+		description: 'Shoe sole tip!',
+	},
+	{
+		x1: 20,
+		x2: 30,
+		y1: 50,
+		y2: 60,
+		description: 'Textured and patterned outsole',
+	},
+	{
+		x1: 65,
+		x2: 75,
+		y1: 65,
+		y2: 75,
+		description: 'Textured outsole with a stacked heel',
+	},
+]
     let data = englishData; // default to english
     switch(this.props.settings.language)
     {
@@ -22,22 +55,40 @@ class LessonPage7 extends Component {
         data = englishData;
     }
     return (
-      <ScrollView>
+      // <View>
+      //
+      // <Card>
+      //   <CardSection>
+      //     <TitleFont>{data.description.title}</TitleFont>
+      //   </CardSection>
+      //   <CardSection>
+      //     <Text>{data.description.title1}{data.description.body1}{data.description.body2}{data.description.body3}{data.description.body4}</Text>
+      //   </CardSection>
+      //   <CardSection>
+      //     <Text>{data.description.title2}{data.description.body5}{data.description.body6}{data.description.body7}{data.description.body8}</Text>
+      //   </CardSection>
+      // </Card>
+      //
+      //
+      //
+      //
+      <ZoomableImage
+					source={ require('./bill_1.png') }
+					imageHeight={ 600 }
+					imageWidth={ 450 }
+					annotations={ annotations }
+					popOverStyles={ { backgroundColor: 'white' } }
+      />
 
-      <Card>
-        <CardSection>
-          <TitleFont>{data.description.title}</TitleFont>
-        </CardSection>
-        <CardSection>
-          <Text>{data.description.title1}{data.description.body1}{data.description.body2}{data.description.body3}{data.description.body4}</Text>
-        </CardSection>
-        <CardSection>
-          <Text>{data.description.title2}{data.description.body5}{data.description.body6}{data.description.body7}{data.description.body8}</Text>
-        </CardSection>
-      </Card>
+      // <ZoomableImage
+		// 			source={ require('./bill_2.png') }
+		// 			imageHeight={ 600 }
+		// 			imageWidth={ 450 }
+		// 			annotations={ annotations }
+		// 			popOverStyles={ { backgroundColor: 'white' } }
+      // />
 
-
-      </ScrollView>
+      // </View>
     );
   }
 
