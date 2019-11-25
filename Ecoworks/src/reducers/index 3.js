4f806e0b789e0b801fd660e8718eb5e74241df6b
Alex Song <songya@umich.edu>
2019-11-24 21:17:51 -0500

    remoeved a bunch of unecessary files

--- Ecoworks/src/reducers/index 3.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import TabNavigatorReducer from './TabNavigatorReducer';
-import SettingsReducer from './SettingsReducer';
-
-export default combineReducers({
-  tabNavigatorPage: TabNavigatorReducer,
-  settings: SettingsReducer
-});
