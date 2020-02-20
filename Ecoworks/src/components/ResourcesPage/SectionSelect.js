17aaff0da0bde4abe560bcf76889721100cc0016
eishiau <eishiau@umich.edu>
2020-02-19 21:42:03 -0500

    resources

--- Ecoworks/src/components/ResourcesPage/SectionSelect.js
+++ Ecoworks/src/components/ResourcesPage/SectionSelect.js
@@ -4,7 +4,11 @@ import {
     Text,
     View,
     FlatList
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Modal
 } from 'react-native';
+
 
 import { CardSection, Card, TitleFont } from '../common';
 
@@ -16,16 +20,17 @@ import waterData from './water.json';
 import gasDataSpanish from './gas_elec_spanish.json';
 import healthDataSpanish from './health_safety_spanish.json';
 import waterDataSpanish from './water_spanish.json';
+import ResourceListItem from './ResourceListItem.js';
 
 import { connect } from 'react-redux';
 
 
 select() {
-  if (this.state.selected === "gas/elec") {
+  if (this.state.selected === "gas_elec") {
        return(<ResourceListItem resourcetype = {"gas/elec"}/>);
   }
-  else if (this.state.selected === "health/safety") {
-        return (<ResourceListItem resourcetype = {"health/safety"}/>);
+  else if (this.state.selected === "health_safety") {
+        return (<ResourceListItem resourcetype = {"health_safety"}/>);
   }
   else if (this.state.selected === "water") {
         return (<ResourceListItem resourcetype = {"water"}/>);
@@ -35,6 +40,28 @@ select() {
   render() {      
      return (
         <View>
+
+        <TouchableOpacity onPress={this.setState({selected: "gas_elec"})}>
+        <Card>
+        <CardSection> <Text> Gas and Electricity </Text>
+        </CardSection>
+        </Card>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.setState({selected: "health_safety"})}>
+        <Card>
+        <CardSection> <Text> Health and Safety </Text>
+        </CardSection>
+        </Card>
+         </TouchableOpacity>
+
+        <TouchableOpacity onPress={this.setState({selected: "water"})}>
+        <Card>
+        <CardSection> <Text> Water </Text>
+        </CardSection>
+        </Card>
+        </TouchableOpacity>
+
         {this.select()}
         </View>
       );
